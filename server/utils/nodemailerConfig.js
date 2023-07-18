dc6685cb1cf8fc800051de416745f2cbfc7f19cd
Abiral <abiralkc21@gmail.com>
2023-07-18 15:54:46 +0545

    Tokenization and Cookies setup

---
+++ server/utils/nodemailerConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            // user: 'katelin.sporer47@ethereal.email',
+            // pass: 'h9ZQKT5C1P6pPC45yN'
+            user: 'marilou61@ethereal.email',
+            pass: 'UQFQTRtDHw7p27kMGm'
+
+           
+        }
+    
+}
